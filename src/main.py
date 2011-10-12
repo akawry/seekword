d8900ace12fe05533af71b85d2423854f1aa550b
Alex Kawrykow <akawry@cs.mcgill.ca>
2011-10-11 21:56:02 -0400

    using dict2xml for xml serialization. added basic user stats

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.api import memcache
-from models import Level
+from django.utils import simplejson
+from models import Level, User, Submission
+from dict2xml import dict2xml
 
 class LevelHandler(webapp.RequestHandler):
 
@@ -10,18 +12,49 @@ class LevelHandler(webapp.RequestHandler):
         level = memcache.get('level')
         if level is None:
             level = Level.all().get()
+            memcache.set('level', level)
         
         if level is not None:
-            suffix = self.request.path.split(".")[1]
-            suffix = suffix.lower()
-    
-            if suffix == "json":
+            format = self.request.get('format', 'json').lower()
+            
+            if format == "json":
                 self.response.out.write(level.to_json())
-            elif suffix == "xml":
+            elif format == "xml":
                 self.response.out.write(level.to_xml())
-        
+
+class ScoreHandler(webapp.RequestHandler):
     
-application = webapp.WSGIApplication([('/level.*', LevelHandler)], debug=True)
+    def get_user_stats(self, user, format):
+        
+        played = 0
+        submissions = user.submission_set
+        if submissions is not None:
+            played = submissions.count()
+        
+        res = {"response": {"user" : user.name, "played" : played}}
+        
+        if format == "json":
+            res = simplejson.dumps(res)
+        
+        elif format == "xml":
+            res = dict2xml(res).to_string()
+            
+        return res
+            
+    def get(self):
+        user = self.request.get('user', None)
+        if user is not None:
+            user = User.all().filter("name =", user).get()
+            
+            if user is not None:
+                format = self.request.get('format', 'json').lower()
+                res = self.get_user_stats(user, format)
+                self.response.out.write(res)
+                
+                        
+    
+application = webapp.WSGIApplication([('/level', LevelHandler),
+                                      ('/score', ScoreHandler)], debug=True)
 
 
 def main():
--- src/models.py
+++ src/models.py
@@ -1,24 +1,38 @@
 from google.appengine.ext import db
 from django.utils import simplejson
+from dict2xml import dict2xml
 
 class Level(db.Model):
+    ''' Defines a level'''
+    
+    # the word grid as a string. wrapped around by the client  
     grid = db.StringProperty()
+    # list of words available to be found in the grid 
     word_bank = db.StringListProperty()
-    time = db.DateTimeProperty(auto_now = True)
+    # when this level is active 
+    time = db.DateTimeProperty()
     
     def to_xml(self):
-        buff = "<response>"
-        buff += "<grid>" + self.grid + "</grid>"
-        buff += "<word_bank>"
-        for word in self.word_bank:
-            buff += "<word>" + word + "</word>"
-        buff += "</word_bank>"
-        buff += "<time>" + str(self.time.utcnow()) + "</time>"
-        buff += "</response>"
-        return buff
+        obj = {"response" : {"grid": self.grid, "word_bank" : {"word" : self.word_bank}, "time" : str(self.time.utcnow())}}
+        return dict2xml(obj).to_string()
     
     def to_json(self):
-        obj = {"grid": self.grid,
-               "word_bank" : self.word_bank,
-               "time" : str(self.time.utcnow())}
+        obj = {"response" : {"grid": self.grid, "word_bank" : self.word_bank, "time" : str(self.time.utcnow())}}
         return simplejson.dumps(obj)
+    
+    
+class User(db.Model):
+    ''' User for keeping track of scores and submissions '''
+    name = db.StringProperty()
+
+class Submission(db.Model):
+    ''' When a level is completed by a user '''
+    
+    # submitting user 
+    user = db.ReferenceProperty(User)
+    # score achieved for submission 
+    score = db.IntegerProperty()
+    # time of submission 
+    time = db.DateTimeProperty()
+    # list of words found in the session 
+    words_found = db.StringListProperty()
